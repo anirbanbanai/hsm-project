e97d563da97d799f019125e062abb896f1fd2741
Anirban <anirbanchondrobanai@gmail.com>
2023-08-25 13:45:50 +0600

    problem solve 2

--- src/app/(allrou)/page.tsx
+++ src/app/(allrou)/page.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 
 
-export default function Homes({ children }: { children: React.ReactNode }) {
+ function Homes({ children }: { children: React.ReactNode }) {
+
+  const [data, setdata] = useState();
+    useEffect(() => {
+        fetch("data.json")
+            .then(res => res.json())
+            .then(data => {
+                setdata(data)
+            })
+    }, [])
+    console.log(data);
   return (
     <main>
       {children}
     </main>
   )
 }
+
+export default Homes;
